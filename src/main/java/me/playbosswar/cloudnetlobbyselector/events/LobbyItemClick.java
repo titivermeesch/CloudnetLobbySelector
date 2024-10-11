c939df73cb56e7a7426ac51494a46d0737dee32d
Tristan Vermeesch <titivermeesch@gmail.com>
2024-10-11 14:38:42 +0200

    wip

--- src/main/java/me/playbosswar/cloudnetlobbyselector/events/LobbyItemClick.java
+++ src/main/java/me/playbosswar/cloudnetlobbyselector/events/LobbyItemClick.java
@@ -1,7 +1,9 @@
 package me.playbosswar.cloudnetlobbyselector.events;
 
+import me.playbosswar.cloudnetlobbyselector.Main;
 import me.playbosswar.cloudnetlobbyselector.gui.LobbySelectorMenu;
 import org.bukkit.Material;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -14,12 +16,17 @@ public class LobbyItemClick implements Listener {
         Player p = e.getPlayer();
         ItemStack item = e.getItem();
 
-        if(item == null || item.getType().equals(Material.AIR) || item.getItemMeta() == null) {
+        if (item == null || item.getType().equals(Material.AIR) || item.getItemMeta() == null) {
             return;
         }
 
-        if(item.getItemMeta().getCustomModelData() == 56789876) {
-            p.openInventory(LobbySelectorMenu.getLobbySelectorMenu(p));
+        FileConfiguration c = Main.getInstance().getConfig();
+        int slot = c.getInt("joinItemSlot");
+
+        if (e.getPlayer().getInventory().getHeldItemSlot() != slot) {
+            return;
         }
+
+        p.openInventory(LobbySelectorMenu.getLobbySelectorMenu(p));
     }
 }
